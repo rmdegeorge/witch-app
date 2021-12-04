8acef05421e216b6760ff42b3d9cb275a935d39b
Rob Degeorge <rob.d@puprle.com>
2021-12-04 10:29:27 -0700

    added bones of app

--- client/src/App.js
+++ client/src/App.js
@@ -3,41 +3,41 @@ import {Home, LoginPage, SignupPage, FormulaCalculator} from "./views";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        {/* Extract to a NavBar component */}
-        <nav> 
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/formula-calculator">Formula Calculator</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/signup">
-          <SignupPage />
-        </Route>
-        <Route path="/formula-calculator">
-          <FormulaCalculator />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+  <BrowserRouter>
+    <div>
+    {/* Extract to a NavBar component */}
+    <nav> 
+      <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <Link to="/signup">Sign Up</Link>
+      </li>
+      <li>
+        <Link to="/formula-calculator">Formula Calculator</Link>
+      </li>
+      </ul>
+    </nav>
+    </div>
+    <Switch>
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route path="/login">
+      <LoginPage />
+    </Route>
+    <Route path="/signup">
+      <SignupPage />
+    </Route>
+    <Route path="/formula-calculator">
+      <FormulaCalculator />
+    </Route>
+    </Switch>
+  </BrowserRouter>
   );
 }
 
