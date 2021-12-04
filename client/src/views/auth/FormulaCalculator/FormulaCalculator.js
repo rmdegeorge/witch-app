bbe69da540a477c085351d3c226d27df35d4f368
Rob Degeorge <rob.d@puprle.com>
2021-12-04 10:20:02 -0700

    added FormulaCalculator component

--- client/src/views/auth/FormulaCalculator/FormulaCalculator.js
+++ client/src/views/auth/FormulaCalculator/FormulaCalculator.js
@@ -1,6 +1,12 @@
+import NumberInput from "../../../components/shared/forms/NumberInput/NumberInput";
+import TextInput from "../../../components/shared/forms/TextInput/TextInput";
 
 export default function FormulaCalculator() {
-    return (
-        <h1>Formula Calculator Page</h1>
-    )
+  return (
+    <>
+      <h1>Formula Calculator Page</h1>
+      <TextInput id='herb-1' name='Herb 1' placeholder="Herb Name" labelTxt='Herb 1 Name' />
+      <NumberInput id='herb-1-percent' name='Herb 1 Percent' labelTxt='Herb 1 Percent of Total' placeholder='Herb 1 Percent of Total Volume' />
+    </>
+  );
 }
---
+++ client/src/views/auth/FormulaCalculator/FormulaCalculator.test.js
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom';
+import FormulaCalculator from './FormulaCalculator';
+import {render} from '@testing-library/react';
+/**
+ * Inputs: herb, percent of total, total volume
+ * Outputs: volume of each herb, percent remaining
+ */
+
+describe('Formula Calculator page', () => {
+  it('should ', () => {
+
+  });
+});
